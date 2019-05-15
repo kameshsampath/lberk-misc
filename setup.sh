fed3b8ccd75c0e65e8bce0aefab9c4ee99aa121d
Matthias Wessendorf <mwessend@redhat.com>
2019-05-15 15:16:05 +0200

    Remove Strimzi

--- setup.sh
+++ setup.sh
@@ -15,7 +15,6 @@ else
   reset=''
 fi
 
-strimzi_version=`curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null`
 serving_version="v0.6.0"
 eventing_version="v0.6.0"
 eventing_sources_version="v0.6.0"
@@ -33,7 +32,6 @@ function header_text {
 
 header_text             "Starting Knative on minikube!"
 header_text "Using Kubernetes Version:               ${kube_version}"
-header_text "Using Strimzi Version:                  ${strimzi_version}"
 header_text "Using Knative Serving Version:          ${serving_version}"
 header_text "Using Knative Eventing Version:         ${eventing_version}"
 header_text "Using Knative Eventing Sources Version: ${eventing_sources_version}"
@@ -42,17 +40,6 @@ header_text "Using Istio Version:                    ${istio_version}"
 minikube start --memory="${MEMORY:-12288}" --cpus="${CPUS:-4}" --kubernetes-version="${kube_version}" --vm-driver="${DRIVER:-kvm2}" --disk-size="${DISKSIZE:-30g}" --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
 header_text "Waiting for core k8s services to initialize"
 sleep 5; while echo && kubectl get pods -n kube-system | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
-
-header_text "Strimzi install"
-kubectl create namespace kafka
-curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
-  | sed 's/namespace: .*/namespace: kafka/' \
-  | kubectl -n kafka apply -f -
-
-header_text "Applying Strimzi Cluster file"
-kubectl -n kafka apply -f "https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/${strimzi_version}/examples/kafka/kafka-persistent-single.yaml"
-header_text "Waiting for Strimzi to become ready"
-sleep 5; while echo && kubectl get pods -n kafka | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 
 header_text "Setting up istio"
 kubectl apply --filename "https://raw.githubusercontent.com/knative/serving/${serving_version}/third_party/istio-${istio_version}/istio-crds.yaml" &&
