a65422b2af8c47c5abdf49734c567d023fb3086f
Matthias Wessendorf <mwessend@redhat.com>
2019-08-20 14:24:39 +0200

    latest OLM and istio

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ else
   reset=''
 fi
 
-olm_version="0.10.0"
+olm_version="0.11.0"
 kube_version="v1.13.4"
 
 MEMORY="$(minikube config view | awk '/memory/ { print $3 }')"
@@ -31,7 +31,7 @@ header_text             "Starting OLM on minikube!"
 header_text "Using Kubernetes Version:               ${kube_version}"
 header_text "Using OLM Version:                      ${olm_version}"
 
-minikube start --memory="${MEMORY:-12288}" --cpus="${CPUS:-4}" --kubernetes-version="${kube_version}" --vm-driver="${DRIVER:-kvm2}" --disk-size="${DISKSIZE:-30g}" --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
+minikube start --memory="${MEMORY:-12288}" --cpus="${CPUS:-8}" --kubernetes-version="${kube_version}" --vm-driver="${DRIVER:-kvm2}" --disk-size="${DISKSIZE:-30g}" --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
 header_text "Waiting for core k8s services to initialize"
 sleep 5; while echo && kubectl get pods -n kube-system | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 
@@ -39,3 +39,9 @@ header_text "OLM install"
 kubectl apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/${olm_version}/crds.yaml
 kubectl apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/${olm_version}/olm.yaml
 sleep 5; while echo && kubectl get pods -n olm | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
+
+header_text "Istio install"
+curl -L "https://raw.githubusercontent.com/knative/serving/v0.8.0/third_party/istio-1.1.7/istio-lean.yaml" \
+    | sed 's/LoadBalancer/NodePort/' \
+    | kubectl apply --filename -
+
